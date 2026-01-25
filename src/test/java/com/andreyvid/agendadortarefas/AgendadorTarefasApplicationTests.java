6686b81b34144a4e1e737ca9ff583c629ff6f18d
Andreyvid Dutra <andreyviddutra@gmail.com>
2026-01-25 15:50:56 -0300

    commit inicial com esqueleto

---
+++ src/test/java/com/andreyvid/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreyvid.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
